dde0d30f6462125bd015f5cc604ade57d46eba87
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2020-10-14 18:09:23 -0500

    eval ssh agent

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,8 +17,26 @@ var enemyAttack = 12;
 // fight function
 var fight = function (enemyName) {
 	// let user know they have begun the fight
-	// window.alert("Welcome to Robot Gladiators!");
 	while (enemyHealth > 0 && playerHealth > 0) {
+		if (playerHealth > 0) {
+			// let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+			window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+
+			// pick new enemy to fight based on the index of the enemyNames array
+			var pickedEnemyName = enemyNames[i];
+
+			// reset enemyHealth before starting new fight
+			enemyHealth = 50;
+
+			// use debugger to pause script from running and check what's going on at that moment in the code
+			// debugger;
+
+			// pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+			fight(pickedEnemyName);
+		} else {
+			window.alert('You have lost your robot in battle! Game over!');
+			break;
+		}
 		// ask user if they'd liked to fight or run
 		var promptFight = window.prompt(
 			'Would you like FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.'
