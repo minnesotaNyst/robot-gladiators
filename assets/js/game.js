cc8064b89a574f20f52def1d6a3b257a4b7cc4aa
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2020-10-15 10:36:13 -0500

    reaching out to bcs to troubleshoot

--- assets/js/game.js
+++ assets/js/game.js
@@ -89,8 +89,8 @@ var startGame = function () {
 	// reset player stats
 	playerHealth = 100;
 	playerAttack = 10;
-    playerMoney = 10;
-    
+	playerMoney = 10;
+
 	for (var i = 0; i < enemyNames.length; i++) {
 		if (playerHealth > 0) {
 			// let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
@@ -110,11 +110,31 @@ var startGame = function () {
 		}
 		// in player isn't alive, stop the game
 		else {
-			window.alert('You have lost your robot in battle! Game over!');
+			break;
 		}
 	}
-	//play again
-	//startGame();
+	// after the loop ends, player is either out of health or enemies to fight, so run the endGame function
+	endGame();
 };
 debugger;
 startGame();
+
+var endGame = function () {
+	window.alert("The game has now ended. Let's see how you did!");
+	if (playerHealth > 0) {
+		window.alert(
+			"Great job, you've survived the game! You now have a score of " +
+				playerMoney +
+				'.'
+		);
+	} else {
+		window.alert("You've lost your robot in battle!");
+	}
+	// ask if they would like to play again
+	var playAgainConfirm = window.confirm('Would you like to play again?');
+	if (playAgainConfirm) {
+		startGame();
+	} else {
+		window.alert('Thank you for playing Robot Gladiators! Come back soon!');
+	}
+};
