c2bbce82344d264c3b2764fab3c5fd4e0f1bb5db
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2020-10-16 09:48:21 -0500

    bug fix related to null player names

--- assets/js/game.js
+++ assets/js/game.js
@@ -187,8 +187,20 @@ var randomNumber = function (min, max) {
 
 	return value;
 };
+
+var getPlayerName = function () {
+	var name = '';
+
+	while (name === '' || name === null) {
+		name = prompt("What is your robot's name?");
+	}
+	console.log("Your robot's name is, " + name + '!');
+	return name;
+};
+
+//player info default
 var playerInfo = {
-	name: window.prompt("What is your robot's name?"),
+	name: getPlayerName(),
 	health: 100,
 	attack: 10,
 	money: 10,
@@ -217,6 +229,7 @@ var playerInfo = {
 	}
 };
 
+//enemy info default
 var enemyInfo = [
 	{
 		name: 'Roborto',
